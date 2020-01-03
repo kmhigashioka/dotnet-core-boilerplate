21b82f0903ca4ac4290d8a620316f922932425d8
Kazuhito Higashioka <kmhigashioka@gmail.com>
2020-01-04 00:48:23 +0800

    Refactor dependency injection

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Reflection;
+﻿using Application;
 using IdentityServer4.AccessTokenValidation;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -10,7 +8,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Application.Interfaces;
-using Application.Todos.Commands.NewTodo;
 using Domain.Entities;
 using WebUI.Configuration;
 using AppContext = Persistence.AppContext;
@@ -32,7 +29,7 @@ namespace WebUI
             services.AddDbContext<AppContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc(options => options.EnableEndpointRouting = false)
                 .AddNewtonsoftJson();
-            services.AddMediatR(typeof(NewTodoCommand).GetTypeInfo().Assembly);
+            services.AddApplication();
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<AppContext>()
                 .AddDefaultTokenProviders();
